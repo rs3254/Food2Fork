c2377f4c7e0726a5481d4240206ee42b1aba06e5
Ray Sabbineni <rays@admins-mbp.home>
2016-05-08 16:25:21 -0400

    Initial Commit

---
+++ Food2Fork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Food2Fork
+//
+//  Created by Ray Sabbineni on 5/8/16.
+//  Copyright © 2016 Ray Sabbineni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Food2Fork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Food2Fork
+//
+//  Created by Ray Sabbineni on 5/8/16.
+//  Copyright © 2016 Ray Sabbineni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
